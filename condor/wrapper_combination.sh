5de1de22d6ba17e93b4f5d39e2edb0e8eb1e3e34
Rui Zhang <rui.zhang@cern.ch>
2021-06-01 04:29:30 +0200

    Paraeele nom and fit; submit combine jobs to condor

--- condor/wrapper_combination.sh
+++ condor/wrapper_combination.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cd /afs/cern.ch/work/z/zhangr/HHcomb/hh_combination_fw/hh_combination_fw
-export HH_COMBINATION_FW_MODE="skip_exist";
 
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
 source setup.sh
+export HH_COMBINATION_FW_MODE="skip_exist";
 
 job=$1
 
@@ -22,10 +22,9 @@ elif [[ $job == "combine_ws" ]]; then
     input=$2
     sigtype=$3
     channel=$4
-    output=$5
+    mass=$5
     scheme=$6
-    mass=$7
-    command="HHComb $job -i $input -c $combine_channels"
+    command="HHComb $job -i $input -r $sigtype -c $channel -m ${mass} --unblind"
 else
     echo "command not found": $job
 fi
