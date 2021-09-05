2f6d18bcca5d81880070ccab8b5a5573bc0b9488
Rui Zhang <rui.zhang@cern.ch>
2021-09-05 20:32:09 +0200

    Update gen_asimov for ranking

--- NP_ranking/gen_profiled_asimov.sh
+++ NP_ranking/gen_profiled_asimov.sh
@@ -1,3 +1,7 @@
-quickstats generate_asimov -i ../../../FullRun2Workspaces/batches/v140invfb_20210903_CI/output_mu_unblind/rescaled/nonres/bbyy/0.root --poi xsec_br --poi_val 1.0 --poi_profile 1.0 --conditional_mle --globs_np_matching -o ../../../FullRun2Workspaces/batches/v140invfb_20210903_CI/output_mu_unblind/rescaled/nonres/bbyy/cond_1_asimov_1.root --asimov_name cond_1_asimov_1
-
-HHComb best_fit -i ../../../FullRun2Workspaces/batches/v140invfb_20210903_CI/output_mu_unblind/rescaled/nonres/bbyy/cond_1_asimov_1.root -s conditionalGlobs_1.0 -d cond_1_asimov_1
+for input in ../../../FullRun2Workspaces/batches/v140invfb_20210903_CI/output_mu_unblind/rescaled/nonres/bbyy/ \
+            ../../../FullRun2Workspaces/batches/v140invfb_20210903_CI/output_mu_unblind/rescaled/nonres/bbtautau/ \
+            ../../../FullRun2Workspaces/batches/v140invfb_20210903_CI/output_mu_unblind/combined/nonres/A-bbtautau_bbyy-fullcorr/ ;
+do
+    quickstats generate_asimov -i ${input}/0.root --poi xsec_br --poi_val 0.032776  --poi_profile 0.032776 --conditional_mle --globs_np_matching -o ${input}/cond_1_asimov_1.0.root --asimov_name cond_1_asimov_1
+    HHComb best_fit -i ${input}/cond_1_asimov_1.0.root -s conditionalGlobs_0.032776 -d cond_1_asimov_1
+done
