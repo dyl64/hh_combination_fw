30c33fdd069a8d53ab18792b5d6e5a5227165b96
Rui Zhang <rui.zhang@cern.ch>
2021-11-09 17:33:45 +0100

    Add make indiv for bbyy

--- run_local.sh
+++ run_local.sh
@@ -5,15 +5,15 @@ HHComb process_channels -i ../../FullRun2Workspaces/original/20211106_mu_all/ -c
 HHComb combine_ws -i ../output/v3000invfb_20211106_Local/indiv/ -c bbyy,bbtautau -r nonres --minimizer_options configs/minimizer_fixXSunc.json --file_format "<mass[F]>_kl_<kl[P]>" --scheme configs/np_map_kl_v10.json
 
 
-
 # param
-HHComb process_channels -i ../../FullRun2Workspaces/original/20211106_mu_all/ -c bbyy,bbtautau -r nonres -o ../output/v3000invfb_20211106_Local/param2/ --minimizer_options configs/minimizer.json --config configs/regularization_kl.yaml --no-cache --file_format "<mass[F]>_kl" --param klambda=-2_8_0.2
+HHComb process_channels -i ../../FullRun2Workspaces/original/20211106_mu_all/ -c bbyy,bbtautau -r nonres -o ../output/v3000invfb_20211106_Local/param/ --minimizer_options configs/minimizer.json --config configs/regularization_kl.yaml --no-cache --file_format "<mass[F]>_kl" --param klambda=1
+#HHComb process_channels -i ../../FullRun2Workspaces/original/20211106_mu_all/ -c bbyy,bbtautau -r nonres -o ../output/v3000invfb_20211106_Local/param/ --minimizer_options configs/minimizer.json --config configs/regularization_kl.yaml --no-cache --file_format "<mass[F]>_kl" --param klambda=-2_8_0.2
 
 # for LH
 HHComb combine_ws -i ../output/v3000invfb_20211106_Local/param/ -r nonres -c bbtautau,bbyy --minimizer_options configs/minimizer.json --config configs/regularization_kl.yaml --scheme configs/np_map_kl_v10.json --skip-limit --no-cache --file_format "<mass[F]>_kl" --param klambda=1
 
-# for XS
-HHComb combine_ws -i ../output/v3000invfb_20211106_Local/param/ -r nonres -c bbyy,bbtautau --minimizer_options configs/minimizer_fixXSunc.json --scheme configs/np_map_kl_v10.json --file_format "<mass[F]>_kl" --param klambda=-2_8_0.2
+## for XS
+#HHComb combine_ws -i ../output/v3000invfb_20211106_Local/param/ -r nonres -c bbyy,bbtautau --minimizer_options configs/minimizer_fixXSunc.json --scheme configs/np_map_kl_v10.json --file_format "<mass[F]>_kl" --param klambda=-2_8_0.2
 
 
 # LH scan
