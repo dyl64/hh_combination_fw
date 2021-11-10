4a81b5988b5e6cad26b36a2d64c583661c7ec04c
Rui Zhang <rui.zhang@cern.ch>
2021-11-10 02:56:38 +0100

    CI

--- python_modules/kl_likelihood.py
+++ python_modules/kl_likelihood.py
@@ -19,20 +19,21 @@ from quickstats.components.likelihood import scan_nll
               help='POI to scan. If not specified, the first POI from the workspace is used.')
 @click.option('--cache/--no-cache', default=True, show_default=True,
               help='Cache existing result')
-@click.option('-f', '--fix', 'fix_param', default="mu*=1,mu_*=1,xsec_br=1", show_default=True,
-              help='POI mu in bbtautau ws is floated. Note it is used in RooProduct::SigXsecOverSMVBF_modified and not regularised')
 @click.option('-i', '--input_folder', required=True, help='Path to the task.')
 @click.option('-s', '--scheme',  default='fullcorr', help='correlation scheme (only affect which combined workspace to pick)')
 @click.option('-c', '--channels',  required=True, help='individual channels to include (comma separable); combined channel always included')
+@click.option('--include-chan/--skip-chan', default=True, help='include or skip individual channels')
 def kl_likelihood(**kwargs):
     input_file, poi, channels, scheme = kwargs['input_folder'], kwargs['poi'], kwargs['channels'], kwargs['scheme']
-    fix_param = kwargs['fix_param']
     scan_min, scan_max, scan_step = kwargs['scan_min'], kwargs['scan_max'], kwargs['scan_step']
     outdir = f'{input_file}/likelihood/' # zhangr
 
     channels = sorted(channels.split(','), key=lambda x: (x.casefold(), x.swapcase()))
-    input_files = [f'{input_file}/rescaled/nonres/{channel}/0_kl.root' for channel in channels]
-    outnames = [f'{channel}_{poi}' for channel in channels]
+    if kwargs['include_chan']:
+        input_files = [f'{input_file}/rescaled/nonres/{channel}/0_kl.root' for channel in channels]
+        outnames = [f'{channel}_{poi}' for channel in channels]
+    else:
+        input_files, outnames = [], []
 
     # append combined
     input_files.append(f'{input_file}/combined/nonres/A-{"_".join(channels)}-{scheme}/0_kl.root')
@@ -44,11 +45,13 @@ def kl_likelihood(**kwargs):
     for input_file, outname in zip(input_files, outnames):
         # generate asimov using CLI tool
         output_file = input_file.replace(".root", ".SplusBasimov.root")
+        fix_param = 'xsec_br=1,klambda=1'
         command = f'quickstats generate_standard_asimov -t -2 -p {poi} -d combData -i {input_file} -o {output_file} --fix {fix_param} --snapshot nominalNuis'
         print(command)
         os.system(command)
 
         # scan likelihood using CLI tool
-        command = f'quickstats likelihood_scan -i {output_file} --min {scan_min} --max {scan_max} --step {scan_step} -d asimovData_1_NP_Nominal --parallel -1 --print_level -1 -p {poi} -o {outname} {other_options} --outdir {outdir} --fix {fix_param} --snapshot nominalNuis'
+        fix_param = 'xsec_br=1'
+        command = f'quickstats likelihood_scan -i {output_file} --min {scan_min} --max {scan_max} --step {scan_step} -d asimovData_1_NP_Nominal --parallel -1 --print_level -1 -p {poi} -o {outname} {other_options} --outdir {outdir} --fix {fix_param}'
         print(command)
         os.system(command)
--- run_local.sh
+++ run_local.sh
@@ -13,12 +13,39 @@ HHComb combine_ws -i ../output/v3000invfb_20211106_Local/param/ -r nonres -c bby
 
 
 ## for LH
+HHComb process_channels -i ../../FullRun2Workspaces/original/20211106_mu_all/ -c bbyy,bbtautau -r nonres -o ../output/v3000invfb_20211106_Local/param/ --minimizer_options configs/minimizer.json --config configs/regularization_kl.yaml --no-cache --file_format "<mass[F]>_kl" --param klambda=1 --skip-limit
+
 HHComb combine_ws -i ../output/v3000invfb_20211106_Local/param/ -r nonres -c bbtautau,bbyy --minimizer_options configs/minimizer.json --config configs/regularization_kl.yaml --scheme configs/np_map_kl_v10.json --skip-limit --no-cache --file_format "<mass[F]>_kl" --param klambda=1
 
 
-HHComb process_channels -i ../../FullRun2Workspaces/original/20211106_mu_all/ -c bbyy,bbtautau -r nonres -o ../output/v3000invfb_20211106_Local/param/ --minimizer_options configs/minimizer.json --config configs/regularization_kl.yaml --no-cache --file_format "<mass[F]>_kl" --param klambda=1
+HHComb kl_likelihood -i ../output/v3000invfb_20211106_Local/param/  -c bbyy,bbtautau --min -2 --max 10 --step 0.1 --no-cache
 
-HHComb kl_likelihood -i ../output/v3000invfb_20211106_Local/param/  -c bbyy,bbtautau --min -2 --max 10 --step 0.1
+
+
+
+
+
+
+
+
+##==========
+#HHComb process_channels -i ../../FullRun2Workspaces/original/20211106_mu_all/ -r nonres -c bbyy,bbtautau -o outputs -p klambda=1 --skip-limit --file_format "<mass[F]>_kl" --config configs/regularization_kl.yaml
+#
+#HHComb combine_ws -i outputs/ -r nonres -c bbyy,bbtautau -s configs/np_map_kl_v10.json --config configs/regularization_kl.yaml --skip-limit --file_format "<mass[F]>_kl" -p klambda=1 --skip-limit
+#
+#quickstats generate_standard_asimov -t -2 -p klambda -d combData -i outputs/combined/nonres/A-bbtautau_bbyy-fullcorr/0_kl.root -o 0_kl_asimov.root --fix xsec_br=1,klambda=1 --snapshot nominalNuis
+#
+#quickstats likelihood_scan -i 0_kl_asimov.root --min -2.0 --max 2.0 --step 1.0 -d asimovData_1_NP_Nominal --parallel -1 --print_level -1 -p klambda -o combined_klambda --no-cache --outdir outputs/likelihood/ --fix xsec_br=1
+##==========
+
+
+
+
+
+
+
+
+
 
 
 ## NR
@@ -27,4 +54,8 @@ HHComb pvalue -i ../output/v3000invfb_20211106_CI/NR/rescaled/nonres/bbyy/0.root
 # plotting
 python combine_plot.py -i ../../../output/v3000invfb_20211106_CI/NR/ -sf 1
 python plot_kl.py -i ../../../output/v3000invfb_20211106_Local/indiv/ --config project3000
-python plotting/likelihood/likelihood_plotting.py -a nonres -i ../output/v3000invfb_20211106_Local/param/likelihood/ -c combined_klambda.json -t bbtautau_klambda.json -y bbyy_klambda.json -o ../output/v3000invfb_20211106_Local/param/likelihood/ --threshold 12
+
+
+
+python plotting/likelihood/likelihood_plotting.py -a nonres -i ../output/v3000invfb_20211106_Local/param/likelihood/ -c combined_klambda.json -t bbtautau_klambda.json -y bbyy_klambda.json -o ../output/v3000invfb_20211106_Local/param/figures/ --threshold 12
+
