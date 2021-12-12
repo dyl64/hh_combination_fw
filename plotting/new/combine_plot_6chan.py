6a608397bb80f6798bfa4cf28e0bb1f60a2e8401
Rui Zhang <rui.zhang@cern.ch>
2021-12-12 23:29:19 +0100

    Update plotting

--- plotting/new/combine_plot_6chan.py
+++ plotting/new/combine_plot_6chan.py
@@ -15,22 +15,24 @@ class DataReader(object):
     def __init__(self, args):
         infolder = args.input_folder
         self.json_files_main = {
-            'combined': f'{infolder}/limits/nonres/combined/A-bbtautau_bbyy-fullcorr/0.json',
+            'combined': f'{infolder}/limits/nonres/combined/A-bbtautau_bbyy-fullcorr/mass_0.json',
             'bbyy': f'{infolder}/limits/nonres/bbyy/mass_0.json',
             'bbtautau': f'{infolder}/limits/nonres/bbtautau/mass_0.json',
         }
         if args.all:
-            self.json_files_main.update({
-                'combined': f'{infolder}/limits/nonres/combined/A-bbbb_bbll_bbtautau_bbVV_bbyy_WWWW-fullcorr/0.json',
-                'combined3': f'{infolder}/limits/nonres/combined/A-bbbb_bbll_bbtautau-fullcorr/0.json',
-                'combined5ll': f'{infolder}/limits/nonres/combined//A-bbbb_bbtautau_bbVV_bbyy_WWWW-fullcorr/0.json',
-                'combined5VV': f'{infolder}/limits/nonres/combined/A-bbbb_bbll_bbtautau-fullcorr/0.json',
-                'combined5WW': f'{infolder}/limits/nonres/combined/A-bbbb_bbll_bbtautau-fullcorr/0.json',
-                'bbbb': f'{infolder}/limits/nonres/bbbb/mass_0.json',
+            self.json_files_main = {
+                'combined': f'{infolder}/limits/nonres/combined/A-bbbb_bbll_bbtautau_bbVV_bbyy_WWWW-fullcorr/mass_0.json',
+                'combined5ll': f'{infolder}/limits/nonres/combined//A-bbbb_bbtautau_bbVV_bbyy_WWWW-fullcorr/mass_0.json',
+                'combined5VV': f'{infolder}/limits/nonres/combined/A-bbbb_bbll_bbtautau_bbyy_WWWW-fullcorr/mass_0.json',
+                'combined5WW': f'{infolder}/limits/nonres/combined/A-bbbb_bbll_bbtautau_bbVV_bbyy-fullcorr/mass_0.json',
                 'bbll': f'{infolder}/limits/nonres/bbll/mass_0.json',
                 'bbVV': f'{infolder}/limits/nonres/bbVV/mass_0.json',
                 'WWWW': f'{infolder}/limits/nonres/WWWW/mass_0.json',
-            })
+                'combined3': f'{infolder}/limits/nonres/combined/A-bbbb_bbtautau_bbyy-fullcorr/mass_0.json',
+                'bbbb': f'{infolder}/limits/nonres/bbbb/mass_0.json',
+                'bbyy': f'{infolder}/limits/nonres/bbyy/mass_0.json',
+                'bbtautau': f'{infolder}/limits/nonres/bbtautau/mass_0.json',
+            }
         self.json_files_addition = {
             'combined': f'{infolder}/stat/limits/nonres/combined/A-bbtautau_bbyy-fullcorr/0.json',
             'bbyy': f'{infolder}/stat/limits/nonres/bbyy/0.json',
@@ -42,6 +44,7 @@ class DataReader(object):
         self.label_map = {
             'bbtautau': r'$\mathrm{b\bar{b}\tau^+\tau^-}$',
             'bbyy': r'$\mathrm{b\bar{b}\gamma\gamma}$',
+            'bbbb': r'$\mathrm{b\bar{b}b\bar{b}}$',
             'bbll': r'$\mathrm{b\bar{b}ll}$',
             'bbVV': r'$\mathrm{b\bar{b}VV}$',
             'WWWW': r'Multilepton',
@@ -49,12 +52,13 @@ class DataReader(object):
             'combined3': r'Top 3 combined',
             'combined5ll': r'N - bbll',
             'combined5VV': r'N - bbVV',
-            'combined5WW': r'N - ML',
+            'combined5WW': r'N - multilepton',
         }
         
         self.analysis_label_options = {'fontsize':30, 'energy': '13 TeV', 
                                   'lumi': '139 fb$^{-1}$',
-                                  'extra_text': r'$\sigma_{ggF+VBF}^{SM}=32.78$ fb'}
+                                  'extra_text': r'$\sigma_{ggF+VBF}^{SM}=32.78$ fb',
+                                  }
         
         self.labels = {
             '2sigma': r'Comb. exp. limit $\pm 2\sigma$',
@@ -67,12 +71,19 @@ class DataReader(object):
                 'tick_bothsides': False,
                 'major_length': 12,
                 },
+            'legend':{
+                'loc': 'upper right',
+                },
+            'figure':{
+                'figsize': (11,15),
+                }
         }
         self.stat = args.stat
 
     def extract_df_from_json(self):
         data_dict = {}
         for chan, json_file in self.json_files_main.items():
+            print(chan, json_file)
             data_dict[chan] = json.load(open(json_file))
         if self.stat:
             for chan in data_dict:
@@ -84,9 +95,11 @@ class DataReader(object):
 def main(args):
     dr = DataReader(args)
     df = dr.extract_df_from_json()
-    plotter = UpperLimit1DPlot(df, dr.label_map, line_below=["bbyy"], labels=dr.labels, analysis_label_options=dr.analysis_label_options, styles=dr.styles)
+    plotter = UpperLimit1DPlot(df, dr.label_map, line_below=["combined3", "combined5ll"] if args.all else ["bbyy"], labels=dr.labels, analysis_label_options=dr.analysis_label_options, styles=dr.styles)
     ax = plotter.draw(logx=False, xlabel=r"95% CL upper limit on signal strength", draw_observed=False, draw_stat=args.stat)
     ax.set_xlim([0, None])
+    if args.xlim:
+        ax.set_xlim([0, args.xlim])
     outfolder = f"{args.input_folder}/figures/"
     makedirs(path.dirname(outfolder), exist_ok=True)
     plt.savefig(f"{outfolder}/{args.output_name}.pdf", bbox_inches="tight")
@@ -101,6 +114,7 @@ if __name__ == '__main__':
     parser.add_argument('-sf', '--sf', default=1000/32.776, type=float, required=False, help='reverse of scale factor applied in regularisation.yaml')
     parser.add_argument('-stat', '--stat', action='store_true', default=False, help='Switch on stat only results')
     parser.add_argument('-a', '--all', action='store_true', default=False, help='Run all 6 channels')
+    parser.add_argument('-x', '--xlim', default=None, type=float, required=False, help='x upper range to plot')
 
     args = parser.parse_args()
     main(args)
