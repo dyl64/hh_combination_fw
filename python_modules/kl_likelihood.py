cc3156285534361ee4bf74cb899ffb4c92f5354b
Avencast <yulei@cern.ch>
2022-04-10 21:55:55 +0200

    update poi and fix parameter

--- python_modules/kl_likelihood.py
+++ python_modules/kl_likelihood.py
@@ -17,6 +17,8 @@ DEFAULT_UNBLIND_DATASET = 'obsData'
               help='\b Parameter name expression describing the internal parameterisation.\n'
                    '\b Example: "klambda=-10_10_0.2,k2v=1"\n'
                    '\b Refer to documentation for more information\n')
+@click.option('-f', '--fix', 'fix_param', default="", show_default=True,
+              help='Parameters to fix')
 @click.option('--config', 'config_file', default=None, show_default=True,
               help='configuration file for task options')
 @click.option('--cache/--no-cache', default=True, show_default=True,
@@ -35,7 +37,7 @@ DEFAULT_UNBLIND_DATASET = 'obsData'
 def kl_likelihood(**kwargs):
     input_file, output, poi, channels, scheme, kl_options, hypo_type = kwargs['input_folder'], kwargs['output'], kwargs[
         'poi'], kwargs['channels'], kwargs['scheme'], kwargs['kl_options'], int(kwargs['hypothesis_type'])
-    param_expr, config_file = kwargs['param_expr'], kwargs['config_file']
+    param_expr, config_file, fix_param = kwargs['param_expr'], kwargs['config_file'], kwargs['fix_param']
     # output = '/'.join([output, 'kl1' if kwargs['splusb'] else 'kl0'])
     output = '/'.join([output, f'kl{hypo_type}'])
     channels = sorted(channels.split(','), key=lambda x: (x.casefold(), x.swapcase()))
@@ -65,15 +67,15 @@ def kl_likelihood(**kwargs):
         outDataSetName = data_name
         if hypo_type != 2:
             output_file = input_file.replace(".root", f'_poi_{hypo_type}' + ".root")
-            fix_param = f'xsec_br=1,{poi}={hypo_type}'
+            fix_params = f'xsec_br=1,{fix_param}'
             asimovDataType = -1 - hypo_type
             outDataSetName = f"asimovData_{hypo_type}_NP_Nominal"
-            command = f'quickstats generate_standard_asimov -t {asimovDataType} -p {poi} -i {input_file} -o {output_file} --fix {fix_param} -d {data_name}'
+            command = f'quickstats generate_standard_asimov -t {asimovDataType} -p xsec_br -i {input_file} -o {output_file} --fix {fix_params} -d {data_name}'
             print(command)
             os.system(command)
 
         # scan likelihood using CLI tool
-        fix_param = 'xsec_br=1'
-        command = f'quickstats likelihood_scan -i {output_file} --param_expr "{param_expr}" -d {outDataSetName} --parallel -1 --print_level 1 -o {out_name} {other_options} --outdir {outdir} --fix {fix_param} {kl_options} '
+        fix_params = 'xsec_br=1'
+        command = f'quickstats likelihood_scan -i {output_file} --param_expr "{param_expr}" -d {outDataSetName} --parallel -1 --print_level 1 -o {out_name} {other_options} --outdir {outdir} --fix {fix_params} {kl_options} '
         print(command)
         os.system(command)
