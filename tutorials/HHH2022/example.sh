395335ca33958a2c323a016c5414f3f340685529
Rui Zhang <rui.zhang@cern.ch>
2022-04-20 04:49:01 +0200

    Run xsec scan

---
+++ tutorials/HHH2022/example.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# input arg:
+#       0: combine workspace
+#       1: run likelihood scan
+run_type=$1
+
+# presetup
+function presetup() {
+    workspace_dir=${hh_combination_fw_path} # work directory (including scripts, config files, correlation schemes...)
+
+    output_name="HHH" # output directory name
+    output_dir="${hh_combination_fw_path}/${output_name}" # the output directory
+    data_dir="${hh_combination_fw_path}/FullRun2Workspaces/original/HHH2022/20220415/" # the data directory
+    run_channel='bbyy,bbtautau,bbbb' # how many channels to combine
+
+    # skip_individual="--skip-chan"
+    skip_individual="--include-chan" # if to run individual results in likelihood scan
+    
+    combine="--combine" # if to run combined results in likelihood scan
+    do_limit="--skip-limit" # don't calculate limit (default: --do-limit)
+    config_file="configs/task_options/HHH2022/nonres_kl_kt_likelihood.yaml" # configuration file
+    correlation_scheme="configs/correlation_schemes/HHH2022/nonres_kl_v11.json"
+    minimizer_crosssection_scan="configs/minimizer_options/fix_xs_uncertainty.json" # fix theory cross section uncertainties for cross section scan
+    minimizer_likelihood_scan="configs/minimizer_options/default.json" # include theory cross section uncertainties for likelihood scan
+    poi="klambda" # no use but leave it here
+    kl_kt_scan_range="klambda=-6_11_0.25,kt=0.8_1.4_0.02" # scan range for two pois
+    kl_scan_range="klambda=-6_11_0.2,kt=1" # scan range for kl only
+    fix_param="klambda=1,kt=1" # fix_parameter for generating asimov data
+}
+
+##### setup #####
+presetup
+
+
+# prepare individual workspace file (regulating and skimming)
+function CombineWorkspace() {
+    echo HHComb process_channels -i "${data_dir}" -o "${output_dir}" -c ${run_channel} -r nonres --file_expr '"'"<mass[F]>_kl"'"' --config ${workspace_dir}/${config_file} "${do_limit}" --param_expr "${kl_kt_scan_range}" #--unblind --minimizer_options "${workspace_dir}/${minimizer_crosssection_scan}"
+    echo HHComb combine_ws -i "${output_dir}" -s ${workspace_dir}/${correlation_scheme} -c ${run_channel} -r nonres --file_expr '"'"<mass[F]>_kl"'"' --config ${workspace_dir}/${config_file}  "${do_limit}" --param_expr "${kl_kt_scan_range}" # --unblind
+    echo
+}
+
+# run likelihood scan on pois (kt, klambda, kV, k2V ...)
+function RunXSScan() {
+    declare -A dataset
+    dataset=( ["bbyy"]="combData" ["combined"]="combData"  ["bbtautau"]="obsData" ["bbbb"]="obsData" )
+    #scan_type=$1
+    #echo "-- Running scan for type ${scan_type}"
+    #echo HHComb kl_likelihood -c "${run_channel}" --param_expr  "${kl_kt_scan_range}" --fix "${fix_param}" -i "${output_dir}" -p "${poi}" --config ${workspace_dir}/${config_file} --no-cache "${combine}" --hypothesis_type "${scan_type}" "${skip_individual}"
+    ch=$1
+
+    if [[ ${ch} == 'combined' ]]; then
+        echo quickstats limit_scan -i ${output_dir}/combined/nonres/A-bbbb_bbtautau_bbyy-fullcorr/0_kl.root --param_expr '"'${kl_scan_range}'"'  -p xsec_br --unblind --outdir ${output_dir}/xsection_scan/combined -d "${dataset[${ch}]}"
+    else
+        echo quickstats limit_scan -i ${output_dir}/rescaled/nonres/${ch}/0_kl.root --param_expr '"'${kl_scan_range}'"'  -p xsec_br --unblind --outdir ${output_dir}/xsection_scan/${ch} -d "${dataset[${ch}]}"
+    fi
+    echo
+}
+
+
+
+CombineWorkspace
+for i in bbyy bbtautau bbbb combined; do
+    RunXSScan $i
+done
