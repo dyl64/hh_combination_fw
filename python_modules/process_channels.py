e79dda1f6261de45f15ced0d56f7ae6253b3b364
Chi Lung Cheng <chi.lung.cheng@cern.ch>
2022-04-20 20:18:57 +0200

    update task config options

--- python_modules/process_channels.py
+++ python_modules/process_channels.py
@@ -1,17 +1,62 @@
+from typing import Dict, List
 import sys
 import os
 import re
 import yaml
 import click
 
-import combiner
-import utils
+from combiner import TaskPipelineWS
 
-DEFAULT_NEW_POI = "xsec_br"
-DEFAULT_BLIND_DATASET = 'asimovData'
-DEFAULT_UNBLIND_DATASET = 'obsData'
-DEFAULT_COMB_DATASET = 'combData'
+kDefaultNewPOI         = "xsec_br"
+kDefaultBlindDataset   = 'asimovData'
+kDefaultUnblindDataset = 'obsData'
+kDefaultCombDataset    = 'combData'
 
+def process_task_config(config:Dict, channels:List, blind:bool=True):
+    if config is None:
+        config = {}
+    poi                 = config.get('poi', {})
+    dataset             = config.get('dataset', {})
+    _extra_pois          = config.get('extra_pois', {})
+    _rescale_poi         = config.get('rescale_poi', {})
+    _redefine_parameters = config.get('redefine_parameters', {})
+    _rename_parameters   = config.get('rename_parameters', {})
+    
+    task_config = {}
+    for channel in channels:
+        task_config[channel] = {}
+        old_poiname = poi.get(channel, None)
+        new_poiname = poi.get('combination', kDefaultNewPOI)
+        channel_dataset = dataset.get(channel, {})
+        comb_dataset    = dataset.get('combination', {})
+        if blind:
+            old_dataname = channel_dataset.get("blind", kDefaultBlindDataset)
+            new_dataname = comb_dataset.get("blind", kDefaultCombDataset)
+        else:
+            old_dataname = channel_dataset.get("unblind", kDefaultUnblindDataset)
+            new_dataname = comb_dataset.get("unblind", kDefaultCombDataset)
+        redefine_parameters = _redefine_parameters.get(channel, None)
+        rescale_poi         = _rescale_poi.get(channel, None) 
+        extra_pois          = _extra_pois.get(channel, None)
+        rename_parameters   = _rename_parameters.get(channel, None)
+        task_options = {
+            "likelihood_scan": config.get('likelihood_scan', None),
+            "calculate_pvalue": config.get('calculate_pvalue', None),
+        }
+        task_config[channel] = {
+            "old_poiname": old_poiname,
+            "new_poiname": new_poiname,
+            "old_dataname": old_dataname,
+            "new_dataname": new_dataname,
+            "blind": blind,
+            "redefine_parameters": redefine_parameters,
+            "rescale_poi": rescale_poi,
+            "extra_pois": extra_pois,
+            "rename_parameters": rename_parameters,
+            "task_options": task_options
+        }
+    return task_config
+                        
 @click.command(name='process_channels')
 @click.option('-i', '--input_dir', required=True, 
               help='Path to the processed workspaces.')
@@ -20,7 +65,7 @@ DEFAULT_COMB_DATASET = 'combData'
               help='Type of analysis (resonant or non-resonant).')
 @click.option('-c', '--channels', default='bbbb,bbtautau,bbyy', show_default=True,
               help='analysis channels (separated by commas)')
-@click.option('-o', '--outdir', default="./output", show_default=True,
+@click.option('-o', '--outdir', "output_dir", default="./output", show_default=True,
               help='output directory')
 @click.option('--file_expr', default="<mass[F]>", show_default=True,
               help='\b File name expression describing the external parameterisation.\n'
@@ -38,11 +83,6 @@ DEFAULT_COMB_DATASET = 'combData'
               help='\b Exclude parameter points by expression.\n'
                    '\b Example: "mass=2*,350,400,450;klambda=1.*,2.*,-1.*,-2.*"\n'
                    '\b Refer to documentation for more information\n')
-@click.option('--scaling_release', default="r999", show_default=True,
-              help='Scaling release (obselete, one should set the value by `rescale_poi` in config/regularization.yaml')
-@click.option('--better_bands/--no-better-bands', 'do_better_bands', default=True, show_default=True,
-              help='Evaluate the proper +1 and +2 sigma limit bands.')
-@click.option('--cl', 'CL', type=float, default=0.95, help='Confidence level.')
 @click.option('--blind/--unblind', default=True, show_default=True,
               help='Perform blind or unblind analysis.')
 @click.option('--config', 'config_file', default=None, show_default=True,
@@ -53,13 +93,11 @@ DEFAULT_COMB_DATASET = 'combData'
               type=click.Choice(["DEBUG", "INFO", "WARNING", "ERROR"], case_sensitive=False),
               help='Verbosity level')
 @click.option('--parallel', type=int, default=-1, show_default=True,
-              help='\b Parallelize job across the N workers.'
+              help='\b Parallelize job across N workers.'
                    '\b Case  0: Jobs are run sequentially (for debugging).\n'
                    '\b Case -1: Jobs are run across N_CPU workers.\n')
 @click.option('--cache/--no-cache', default=True, show_default=True,
               help='Cache existing results.')
-@click.option('--save_summary/--skip_summary', default=False, show_default=True,
-              help='Save limit summary.')
 @click.option('--do-limit/--skip-limit', default=True, show_default=True,
               help='Whether to evaluate limits.')
 @click.option('--do-likelihood/--skip-likelihood', default=False, show_default=True,
@@ -68,66 +106,37 @@ DEFAULT_COMB_DATASET = 'combData'
               help='Whether to evaluate pvalue(s).')
 @click.option('--experimental/--official', default=False, show_default=True,
               help='Whether to use experimental method for workspace modification.')
-def process_channels(input_dir, resonant_type, channels, outdir, file_expr,
-                     param_expr, filter_expr, exclude_expr,
-                     scaling_release, do_better_bands, CL, blind,
-                     config_file, minimizer_options, verbosity, 
-                     parallel, cache, save_summary, do_limit,
-                     do_likelihood, do_pvalue, experimental):
+def process_channels(**kwargs):
+    
+    blind       = kwargs['blind']
+    channels    = kwargs['channels']
+    config_file = kwargs["config_file"]
     
     if config_file is not None:
-        config = yaml.safe_load(open(config_file))
+        with open(config_file, "r") as f:
+            config = yaml.safe_load(f)
     else:
         config = None
-    redefine_parameters = config.get('redefine_parameters', None)
-    rescale_poi = config.get('rescale_poi', None)
-    extra_pois = config.get('extra_pois', None)
-    
-    channels = sorted(channels.split(','), key=lambda x: (x.casefold(), x.swapcase()))
+  
+    channels    = sorted(channels.split(','), key=lambda x: (x.casefold(), x.swapcase()))
+    task_config = process_task_config(config, channels, blind)
+                                            
     for channel in channels:
-        old_poi = None if config is None else config['poi'][channel]
-        new_poi = DEFAULT_NEW_POI if config is None else config['poi']['combination']
-        if blind:
-            old_dataname = DEFAULT_BLIND_DATASET if config is None else config['dataset'][channel]['blind']
-            new_dataname = DEFAULT_COMB_DATASET if config is None else config['dataset']['combination']['blind']
-        else:
-            old_dataname = DEFAULT_BLIND_DATASET if config is None else config['dataset'][channel]['unblind']
-            new_dataname = DEFAULT_COMB_DATASET if config is None else config['dataset']['combination']['unblind']
-        if redefine_parameters is not None:
-            channel_redefine_parameters = redefine_parameters.get(channel, None)
-        else:
-            channel_redefine_parameters = None
-        if rescale_poi is not None:
-            channel_rescale_poi = rescale_poi.get(channel, None)
-        else:
-            channel_rescale_poi = None
-            
-        if extra_pois is not None:
-            channel_extra_pois = extra_pois.get(channel, None)
-        else:
-            channel_extra_pois = None
-
-        if config is None:
-            task_options = None
-        else:
-            task_options = {
-                "likelihood_scan": config.get('likelihood_scan', None),
-                "calculate_pvalue": config.get('calculate_pvalue', None),
-            }
-        pipeline = combiner.TaskPipelineWS(input_dir, outdir, resonant_type, channel, scaling_release,
-                                           old_poi, new_poi, old_dataname, new_dataname,
-                                           redefine_parameters=channel_redefine_parameters, 
-                                           rescale_poi=channel_rescale_poi,
-                                           extra_pois=channel_extra_pois,
-                                           file_expr=file_expr, param_expr=param_expr,
-                                           do_better_bands=do_better_bands, CL=CL,
-                                           blind=blind, minimizer_options=minimizer_options,
-                                           verbosity=verbosity, parallel=parallel, cache=cache,
-                                           save_summary=save_summary, do_limit=do_limit,
-                                           do_likelihood=do_likelihood,
-                                           do_pvalue=do_pvalue,
-                                           task_options=task_options,
-                                           filter_expr=filter_expr,
-                                           exclude_expr=exclude_expr,
-                                           experimental=experimental)
+        task_config[channel]["input_dir"]         = kwargs["input_dir"]
+        task_config[channel]["output_dir"]        = kwargs["output_dir"]
+        task_config[channel]["resonant_type"]     = kwargs["resonant_type"]
+        task_config[channel]["channel"]           = channel
+        task_config[channel]["file_expr"]         = kwargs["file_expr"]
+        task_config[channel]["param_expr"]        = kwargs["param_expr"]
+        task_config[channel]["filter_expr"]       = kwargs["filter_expr"]
+        task_config[channel]["exclude_expr"]      = kwargs["exclude_expr"]
+        task_config[channel]["minimizer_options"] = kwargs["minimizer_options"]
+        task_config[channel]["verbosity"]         = kwargs["verbosity"]
+        task_config[channel]["parallel"]          = kwargs["parallel"]
+        task_config[channel]["cache"]             = kwargs["cache"]
+        task_config[channel]["do_limit"]          = kwargs["do_limit"]
+        task_config[channel]["do_likelihood"]     = kwargs["do_likelihood"]
+        task_config[channel]["do_pvalue"]         = kwargs["do_pvalue"]
+        task_config[channel]["experimental"]      = kwargs["experimental"]
+        pipeline = TaskPipelineWS(**task_config[channel])
         pipeline.run_pipeline()
