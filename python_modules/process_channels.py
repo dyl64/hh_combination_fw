c69fc1e0d2d420fa9e663741e67141ffccee174c
Rui Zhang <rui.zhang@cern.ch>
2022-09-22 18:39:38 +0200

    Change experimental to default (no WSCombiner) is required

--- python_modules/process_channels.py
+++ python_modules/process_channels.py
@@ -119,7 +119,7 @@ def process_task_config(config:Dict, channels:List, blind:bool=True):
               help='Whether to run likelihood scan.')
 @click.option('--do-pvalue/--skip-pvalue', default=False, show_default=True,
               help='Whether to evaluate pvalue(s).')
-@click.option('--experimental/--official', default=False, show_default=True,
+@click.option('--experimental/--official', default=True, show_default=True,
               help='Whether to use experimental method for workspace modification.')
 def process_channels(**kwargs):
     
