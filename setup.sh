bdd34a4b763e0b33142c878a89bb752a415f6e17
Chi Lung Cheng <chi.lung.cheng@cern.ch>
2023-07-08 17:48:57 +0200

    update setup script to include support for SWAN environment and remove dependency on workspaceCombiner

--- setup.sh
+++ setup.sh
@@ -1,92 +1,118 @@
 #!/bin/bash
+
 # Further clean up should make them set at proper places and time (Rui Zhang)
 
 export HH_COMBINATION_FW_MODE="overwrite" #"skip_exist";
 echo $HH_COMBINATION_FW_MODE
-unset hh_combination_fw_path
-export hh_combination_fw_path="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# Setup workspaceCombiner
-# Copied from workspaceCombiner/setup.sh (Rui Zhang)
-export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase # use your path
-source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
-if [[ $1 == '98' ]]; then
-    echo 'setup LCG_98python3'
-    lsetup "views LCG_98python3 x86_64-centos7-gcc8-opt"
-elif [[ $1 == '100' ]]; then
-    echo 'setup LCG_100'
-    lsetup "views LCG_100 x86_64-centos7-gcc8-opt"
-elif [[ $1 == '101' ]]; then
-    echo 'setup LCG_101, ROOT 6.24/06'
-    lsetup "views LCG_101 x86_64-centos7-gcc8-opt"
-elif [[ $1 == '102' ]] || [[ -z $1 ]]; then # The default
-    echo 'setup LCG_102, ROOT 6.26/04'
-    lsetup "views LCG_102 x86_64-centos7-gcc11-opt"
-elif [[ $1 == '102b' ]]; then
-    echo 'setup LCG_102b, ROOT 6.26/08'
-    lsetup "views LCG_102b x86_64-centos7-gcc11-opt"
+# check if inside SWAN setup
+if [[ -z "${SWAN_HOME}" ]] && [[ -z "${USER_ENV_SCRIPT}" ]]; then
+    if [ "$#" -ge 1 ];
+    then
+        EnvironmentName=$1
+    else
+        EnvironmentName="103"
+    fi
+    
+    SOURCE="${BASH_SOURCE[0]}"
+    while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+      DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+      SOURCE="$(readlink "$SOURCE")"
+      [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    done
+
+    export hh_combination_fw_path="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+    # Setup workspaceCombiner
+    # Copied from workspaceCombiner/setup.sh (Rui Zhang)
+    export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase # use your path
+    source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+    if [[ $EnvironmentName == '98' ]]; then
+        echo 'setup LCG_98python3'
+        lsetup "views LCG_98python3 x86_64-centos7-gcc8-opt"
+    elif [[ $EnvironmentName == '100' ]]; then
+        echo 'setup LCG_100'
+        lsetup "views LCG_100 x86_64-centos7-gcc8-opt"
+    elif [[ $EnvironmentName == '101' ]]; then
+        echo 'setup LCG_101, ROOT 6.24/06'
+        lsetup "views LCG_101 x86_64-centos7-gcc8-opt"
+    elif [[ $EnvironmentName == '102' ]] || [[ -z $1 ]]; then # The default
+        echo 'setup LCG_102, ROOT 6.26/04'
+        lsetup "views LCG_102 x86_64-centos7-gcc11-opt"
+    elif [[ $EnvironmentName == '102b' ]]; then
+        echo 'setup LCG_102b, ROOT 6.26/08'
+        lsetup "views LCG_102b x86_64-centos7-gcc11-opt"
+    else
+        echo 'Specify a relase number for LCG, default LCG_102'
+        echo 'source setup.sh [98|100|101]'
+        return
+    fi
+    
 else
-    echo 'Specify a relase number for LCG, default LCG_102'
-    echo 'source setup.sh [98|100|101]'
-    return
+  EnvironmentName="swan"
+  export hh_combination_fw_path=$(dirname "$USER_ENV_SCRIPT")
 fi
 
-# More memory
-ulimit -S -s unlimited
+ # More memory
+ ulimit -S -s unlimited
+ 
+ # Greet the user
+ if [ $_DIRCOMB ]; then
+     echo _DIRCOMB is already defined, use a clean shell
+     return 0
+ fi
+ 
+ export _DIRCOMB=$hh_combination_fw_path
+ 
+ # The Makefiles depend only on the root-config script to use ROOT,
+ # so make sure that is available
+ if [[ `which root-config` == "" ]]; then
+     echo "Error: ROOT environment doesn't seem to be configured!"
+ fi
 
-# Greet the user
-if [ $_DIRCOMB ]; then
-    echo _DIRCOMB is already defined, use a clean shell
-    return 0
-fi
-
-# speficy the SFRAME base directory, i.e. the directory in which this file lives
-export _DIRCOMB=${hh_combination_fw_path}/submodules/workspaceCombiner
-
-
-# Modify to describe your directory structure. Default is to use the a structure where
-# all directories are below the SFrame base directory specified above
-export _BIN_PATH=${_DIRCOMB}/build
-export _LIB_PATH=${_DIRCOMB}/build
-
-# The Makefiles depend only on the root-config script to use ROOT,
-# so make sure that is available
-if [[ `which root-config` == "" ]]; then
-    echo "Error: ROOT environment doesn't seem to be configured!"
-fi
-
-if [[ `root-config --platform` == "macosx" ]]; then
-    export DYLD_LIBRARY_PATH=${_LIB_PATH}:${DYLD_LIBRARY_PATH}
-else
-    export LD_LIBRARY_PATH=${_LIB_PATH}:${LD_LIBRARY_PATH}
-fi
-export PATH=${_BIN_PATH}:${PATH}
-cd ${hh_combination_fw_path}
-
-# Copy from workspaceCombiner/setup.sh done
+# # remove dependence on workspaceCombiner
+# # speficy the SFRAME base directory, i.e. the directory in which this file lives
+# export _DIRCOMB=${hh_combination_fw_path}/submodules/workspaceCombiner
+# 
+# 
+# # Modify to describe your directory structure. Default is to use the a structure where
+# # all directories are below the SFrame base directory specified above
+# export _BIN_PATH=${_DIRCOMB}/build
+# export _LIB_PATH=${_DIRCOMB}/build
+# 
+# # The Makefiles depend only on the root-config script to use ROOT,
+# # so make sure that is available
+# if [[ `which root-config` == "" ]]; then
+#     echo "Error: ROOT environment doesn't seem to be configured!"
+# fi
+# 
+# if [[ `root-config --platform` == "macosx" ]]; then
+#     export DYLD_LIBRARY_PATH=${_LIB_PATH}:${DYLD_LIBRARY_PATH}
+# else
+#     export LD_LIBRARY_PATH=${_LIB_PATH}:${LD_LIBRARY_PATH}
+# fi
+# export PATH=${_BIN_PATH}:${PATH}
+# cd ${hh_combination_fw_path}
+# WORKSPACECOMBINER_PATH=${hh_combination_fw_path}/submodules/workspaceCombiner/
+# export WORKSPACECOMBINER_PATH
 
 
 # Setup RooFitExtensions
-cd submodules/RooFitExtensions
+cd ${hh_combination_fw_path}/submodules/RooFitExtensions
+
 if [[ -f build/setup.sh ]]; then
     echo "submodules/RooFitExtensions/build/setup.sh"
     source build/setup.sh
 fi
-cd ${hh_combination_fw_path}
 
-PYTHONPATH=$PYTHONPATH:${hh_combination_fw_path}/python_modules/
+export ROOT_INCLUDE_PATH=$ROOT_INCLUDE_PATH:${hh_combination_fw_path}/submodules/RooFitExtensions
 
-WORKSPACECOMBINER_PATH=${hh_combination_fw_path}/submodules/workspaceCombiner/
-export WORKSPACECOMBINER_PATH
-
-#LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ROOSTATPATH/lib
-ROOT_INCLUDE_PATH=$ROOT_INCLUDE_PATH:${hh_combination_fw_path}/submodules/RooFitExtensions
-export LD_LIBRARY_PATH
-export ROOT_INCLUDE_PATH
-
+# setup hh combination fw
+export PYTHONPATH=$PYTHONPATH:${hh_combination_fw_path}/python_modules/
 export PATH=${hh_combination_fw_path}/bin:$PATH
-
 
 # setup quickstats
 export PATH=${hh_combination_fw_path}/submodules/quickstats/bin:$PATH
 export PYTHONPATH=${hh_combination_fw_path}/submodules/quickstats:$PYTHONPATH
+
+cd ${hh_combination_fw_path}
