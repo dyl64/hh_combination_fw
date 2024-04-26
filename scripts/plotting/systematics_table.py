02fec6d32c75aafd243eac9423fd5c713ad2adbc
Sebastien Rettie <sebastien.rettie@cern.ch>
2024-04-26 12:48:02 +0200

    add table printing

--- scripts/plotting/systematics_table.py
+++ scripts/plotting/systematics_table.py
@@ -9,25 +9,42 @@ SYST_BLOCKS = {
     "stat_only": ["ATLAS_*", "THEO_*"],
     "bbbb": ["ATLAS_bbbb_*"],
     "bbtautau": ["ATLAS_bbtautau_*"],
-    "e_gamma": ["ATLAS_EG_*", "ATLAS_EL_*"],
     "met": ["ATLAS_MET_*"],
-    "ml": ["ATLAS_ml_*"],
-    "prw": ["ATLAS_PU_PRW_DATASF"],
     "theory": ["THEO_*"],
     "ftag": ["ATLAS_FTAG_*", "ATLAS_FT_*"],
     "jet": ["ATLAS_JET_*"],
     "tau": ["ATLAS_TAUS_*"],
+    # "e_gamma": ["ATLAS_EG_*", "ATLAS_EL_*"], # used for merging e_gamma_photon
+    # "ml": ["ATLAS_ml_*"], # used for merging ml_muon
+    # "prw": ["ATLAS_PU_PRW_DATASF"], # used for merging prw_lumi
     # upper error larger than nominal, so merge
-    "bbll": ["ATLAS_bbll_*"],  # merged to bbllyy
-    "bbyy": ["ATLAS_bbyy_*"],  # merged to bbllyy
-    "photon": ["ATLAS_PH_*"],  # merged to e_gamma_photon
-    "muon": ["ATLAS_MUON_*"],  # merged to ml_muon
-    "lumi": ["ATLAS_LUMI_*"],  # merged to prw_lumi
+    # "bbll": ["ATLAS_bbll_*"],  # merged to bbllyy
+    # "bbyy": ["ATLAS_bbyy_*"],  # merged to bbllyy
+    # "photon": ["ATLAS_PH_*"],  # merged to e_gamma_photon
+    # "muon": ["ATLAS_MUON_*"],  # merged to ml_muon
+    # "lumi": ["ATLAS_LUMI_*"],  # merged to prw_lumi
     # merged alternatives
     "e_gamma_photon": ["ATLAS_EG_*", "ATLAS_EL_*", "ATLAS_PH_*"],
     "bbllyy": ["ATLAS_bbll_*", "ATLAS_bbyy_*"],
     "ml_muon": ["ATLAS_ml_*", "ATLAS_MUON_*"],
     "prw_lumi": ["ATLAS_PU_PRW_DATASF", "ATLAS_LUMI_*"],
+}
+TABLE_TITLES = {
+    "nominal": "None (nominal fit)",
+    "fix_all": "Total syst.",
+    "all_syst": "MC stat.",
+    "stat_only": "No MC stat.",
+    "bbbb": r"\bbbb",
+    "bbtautau": r"\bbtt",
+    "met": r"$E_\mathrm{T}^\mathrm{miss}$",
+    "theory": "Theory",
+    "ftag": "FTAG CP",
+    "jet": "Jet CP",
+    "tau": "Tau CP",
+    "e_gamma_photon": r"$e/\gamma$ CP",
+    "bbllyy": r"\bbll and \bbtt",
+    "ml_muon": "Muon CP and \ml",
+    "prw_lumi": "Pile-up reweighting",
 }
 
 
@@ -85,13 +102,50 @@ def run_fits(in_dir, out_dir):
     print(final_cmd)
 
 
+def get_table_start():
+    return (
+        r"\begin{table}[ht]""\n"
+        r"    \centering""\n"
+        r"    \caption{""\n"
+        r"        Impact of systematic uncertainties grouped by similar sources.""\n"
+        r"        The uncertainty on the POI $\mu_{\HH}$ is evaluated with Minos.""\n"
+        r"    }""\n"
+        r"    \resizebox{\columnwidth}{!}{""\n"
+        r"    \begin{tabular}{lcc""\n"
+        r"        S[table-format=4.4, round-mode = figures, round-precision = 4]""\n"
+        r"        S[table-format=4.4, round-mode = figures, round-precision = 4]""\n"
+        r"        S[table-format=4.4, round-mode = figures, round-precision = 4]""\n"
+        r"        }""\n"
+        r"    \toprule""\n"
+        r"    {Group} & {Nuisance parameters fixed} & {POI uncertainty (68\%)} & {Lower bound} & {Upper bound} \\""\n"
+        r"            &                             &                          & {impact (\%)} & {impact (\%)} \\""\n"
+        r"    \midrule""\n"
+    )
+
+
+def get_table_end():
+    return (
+        r"    \bottomrule""\n"
+        r"    \end{tabular}""\n"
+        r"    }""\n"
+        r"    \label{tab:systematics_impact}""\n"
+        r"\end{table}""\n"
+    )
+
+
 def compare_uncertainties(results_path):
+    # prepare final table
+    table = get_table_start()
     err_nom_lo, err_nom_hi = get_err(f"{results_path}/nominal.json")
     for block in SYST_BLOCKS:
         err_block_lo, err_block_hi = get_err(f"{results_path}/{block}.json")
         # subtract in quadrature, see https://twiki.cern.ch/twiki/pub/AtlasProtected/ATLASStatisticsFAQ/pllguide_draft.pdf
-        err_syst_lo = np.sqrt(err_nom_lo**2 - err_block_lo**2)
-        err_syst_hi = np.sqrt(err_nom_hi**2 - err_block_hi**2)
+        err_syst_lo = (
+            100 * np.sqrt(err_nom_lo**2 - err_block_lo**2) / abs(err_nom_lo)
+        )
+        err_syst_hi = (
+            100 * np.sqrt(err_nom_hi**2 - err_block_hi**2) / abs(err_block_hi)
+        )
         print(
             block,
             "uncertainty:",
@@ -101,6 +155,15 @@ def compare_uncertainties(results_path):
             "impact_hi",
             f"{err_syst_hi:.4f}",
         )
+        NPs = r"\texttt{" + ", ".join(SYST_BLOCKS[block]).replace("_", r"\_") + r"}"
+        if block == "nominal":
+            table += f"        {TABLE_TITLES[block]} & N/A & [{err_block_lo:.4f}, {err_block_hi:.4f}] & 100.0 & 100.0 \\\\\n"
+        else:
+            table += f"        {TABLE_TITLES[block]} & {NPs} & [{err_block_lo:.4f}, {err_block_hi:.4f}] & {err_syst_lo:.4f} & {err_syst_hi:.4f} \\\\\n"
+        if block == "fix_all":
+            table += r"    \midrule""\n"
+    table += get_table_end()
+    print(table)
 
 
 if __name__ == "__main__":
